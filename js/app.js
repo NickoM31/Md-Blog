8c9b18a15d5f0a6b8954abd4e7b9ab24367a06f8
Nicko <nicolasmathieu31@hotmail.com>
2016-10-27 16:21:12 +0200

    Md-blog lien entre les deux html

--- js/app.js
+++ js/app.js
@@ -3,53 +3,38 @@
 	var app = {
 
 		init:function(){
-			app.alice();
 			app.json();
-			app.convertir();
-			
 		},
-
-		alice: function(){
-			$.ajax({
-				url : 'http://localhost:2000/alice.md',
-				method : 'GET',
-				datatype : 'html',
-				success : function(response){
-					
-				}
-				
-			})
-		},
-
 		json : function(){
 			$.ajax({
 				url : 'http://localhost:2000/menu.json',
 				method : 'GET',
-				datatype : 'html',
 				success : function(data){
 					var tab = data.menu;
 					for(var i = 0; i < tab.length; i++){
 						var titres = tab[i].title;
 						var pathpath = "http://localhost:2000" + tab[i].path;
-						$("#app").append('<li>'+'<a class="marko"data-info="'+pathpath+'" href="#">'+titres+'</a>'+'</li>');
-						app.convertir();
+						 
+						$("#app").append('<li>'+'<a class="markD"data-info="'+pathpath+'" href="#">'+titres+'</a>'+'</li>');
 					}
+					app.convertir();
 				}
 			})
 		},
 
 		convertir : function(){
-			$(".marko").click(function(){
-				app.data($(this).data('info'));
+			$(".markD").click(function(){
+				var url = $(this).data('info')
+				app.getArticle(url);
 			});
 			
 		},
-		data : function(path){
+		getArticle : function(path){
 			$.ajax({
 				url : path,
 				method : 'GET',
 				success : function(response){
-					var converter = new showdown.Converter(app.response);
+					var converter = new showdown.Converter();
 					var convertHtml = converter.makeHtml(response);
 					$("#md").html(convertHtml);
 				}
---
+++ saisie.js
@@ -0,0 +1,15 @@
+(function(){
+	"use strict";
+	var app = {
+		init:function(){
+			// let's go
+			
+		},
+
+	};
+
+
+	$(document).ready(function(){
+		app.init();
+	});
+})();
